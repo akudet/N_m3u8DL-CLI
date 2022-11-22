614dae4e83874f2a26a236e4da3deab38181df89
Jintong Wu <akudet@qq.com>
2022-11-22 22:53:03 +0800

    Revert "Update ProgressReporter.cs"
    This reverts commit def5cb43917bf838f11dc3b8efc00fed88986768.

--- N_m3u8DL-CLI/ProgressReporter.cs
+++ N_m3u8DL-CLI/ProgressReporter.cs
@@ -14,6 +14,17 @@ namespace N_m3u8DL_CLI
         static object lockThis = new object();
         public static void Report(string progress, string speed)
         {
+            lock (lockThis)
+            {
+                if (!string.IsNullOrEmpty(progress)) ProgressReporter.progress = progress;
+                if (!string.IsNullOrEmpty(speed)) ProgressReporter.speed = speed;
+                string now = DateTime.Now.ToString("HH:mm:ss.000");
+                var sub = Console.WindowWidth - 4 - ProgressReporter.progress.Length - ProgressReporter.speed.Length - now.Length;
+                if (sub <= 0) sub = 0;
+                string print = now + " " + ProgressReporter.progress + " " + ProgressReporter.speed + new string(' ', sub);
+                Console.Write("\r" + print + "\r");
+                //Console.Write(print);
+            }
         }
     }
 }
